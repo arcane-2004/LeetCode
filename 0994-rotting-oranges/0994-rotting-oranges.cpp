defe690bb54eee176bcffd56716629378fd6143b
Sumit Kumar Panika <sumitkp153@gmail.com>
2026-07-19 01:42:26 +0530

    Time: 3 ms (26.48%), Space: 16.7 MB (70.39%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,50 +4,43 @@ public:
         
         int m = grid.size();
         int n = grid[0].size();
+        queue<pair<pair<int, int>, int>> q;
 
         int time = 0;
         int fresh = 0;
-        queue<pair<pair<int, int>, int>> q;
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-
                 if(grid[i][j] == 2){
-                    q.push({{i,j}, 0});
+                    q.push({{i, j}, 0});
                 }
 
-                if(grid[i][j] == 1){
+                else if(grid[i][j] == 1){
                     fresh++;
                 }
-
             }
         }
 
-
         while(!q.empty()){
             auto top = q.front();
+            q.pop();
+
             int row = top.first.first;
             int col = top.first.second;
             int t = top.second;
-            time = max(t, time);
-            q.pop();
+            time = max(time, t);
 
-            //URDL
             int dx[] = {-1, 0, 1, 0};
             int dy[] = {0, 1, 0, -1};
-            
-            
+
             for(int i=0; i<4; i++){
-                
                 int newRow = row + dx[i];
                 int newCol = col + dy[i];
 
                 if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n){
                     if(grid[newRow][newCol] == 1){
-                        q.push({{newRow, newCol}, t+1});
-           
                         grid[newRow][newCol] = 2;
-
+                        q.push({{newRow, newCol}, t+1});
                         fresh--;
                     }
                 }
@@ -59,6 +52,5 @@ public:
         }
 
         return time;
-
     }
 };
