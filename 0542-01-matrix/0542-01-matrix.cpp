3e147bd43bef94c182bd4652e31484f415363b71
Sumit Kumar Panika <sumitkp153@gmail.com>
2026-07-19 02:33:59 +0530

    Time: 19 ms (56.19%), Space: 34.7 MB (77.62%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,43 +4,46 @@ public:
         
         int m = mat.size();
         int n = mat[0].size();
+
         vector<vector<int>> ans(m, vector<int> (n, -1));
 
         queue<pair<int, int>> q;
 
         for(int i=0; i<m; i++){
-            for(int j =0; j<n; j++){
-                if(mat[i][j] == 0){
-                    q.push({i, j});
+            for(int j=0; j<n; j++){
+            
+                if(mat[i][j] == 0 && ans[i][j] == -1){
                     ans[i][j] = 0;
+                    q.push({i, j});
                 }
-            }
+            }    
         }
-
+        
         while(!q.empty()){
             auto top = q.front();
             q.pop();
-
             int row = top.first;
             int col = top.second;
 
-            //URDL
             int dx[] = {-1, 0, 1, 0};
             int dy[] = {0, 1, 0, -1};
 
             for(int i=0; i<4; i++){
-                int nRow = row + dx[i];
-                int nCol = col + dy[i];
+                int newRow = row + dx[i];
+                int newCol = col + dy[i];
 
-                if(nRow >= 0 && nRow < m && nCol >= 0 && nCol < n){
-                    if(mat[nRow][nCol] != 0 && ans[nRow][nCol] == -1){
-                        ans[nRow][nCol] = ans[row][col] + 1;
-                        q.push({nRow, nCol});
+                if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n){
+                    if(mat[newRow][newCol] != 0 && ans[newRow][newCol] == -1){
+                        ans[newRow][newCol] = ans[row][col] + 1;
+                        q.push({newRow, newCol});
                     }
                 }
             }
+            
+
         }
 
-        return ans;
+        return ans;           
+            
     }
 };
