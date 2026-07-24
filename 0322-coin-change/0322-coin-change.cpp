f20c16f087413ab3f6c009ee219be8bd473f015e
Sumit Kumar Panika <sumitkp153@gmail.com>
2026-07-24 12:46:23 +0530

    Time: 32 ms (56.68%), Space: 22 MB (52.71%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -23,27 +23,28 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size() ;
-        vector<vector<int>>dp (n, vector<int> (amount+1, 0));
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
         
         for(int amt = 0; amt <= amount; amt++){
-            if(amt % coins[0] == 0) dp[0][amt] = amt/coins[0];
-            else dp[0][amt] = 1e9;
+            if(amt % coins[0] == 0) prev[amt] = amt/coins[0];
+            else prev[amt] = 1e9;
         }
 
         for(int ind = 1; ind<n; ind++){
             for(int amt =0; amt <= amount; amt++){
-                int notTake = 0 + dp[ind-1][amt];
+                int notTake = 0 + prev[amt];
                 int take = INT_MAX;
                 if(coins[ind] <= amt){
-                    take = 1 + dp[ind][amt-coins[ind]];
+                    take = 1 + curr[amt-coins[ind]];
                 }
 
-                dp[ind][amt] = min(take, notTake);
+                curr[amt] = min(take, notTake);
             }
+            prev = curr;
             
         }
 
-        int ans = dp[n-1][amount];
+        int ans = prev[amount];
 
         if( ans >= 1e9) return -1;
 
