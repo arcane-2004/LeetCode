dfe31db40df241e0462dcfcdb48d806eda23355e
Sumit Kumar Panika <sumitkp153@gmail.com>
2026-08-25 12:18:26 +0530

    Time: 44 ms (57.17%), Space: 65.7 MB (53.45%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,49 +1,40 @@
 class Solution {
+    bool solve(int node, unordered_map<int, bool> &visited, unordered_map<int, bool> &pathVisited, vector<int> &checked, vector<vector<int>>& graph){
+
+        visited[node] = 1;
+        pathVisited[node] = 1;
+
+        for(int i: graph[node]){
+            if(!visited[i]){
+                bool check = solve(i, visited, pathVisited, checked, graph);
+                if(check){
+                    return true; 
+                }
+            }
+            else if(pathVisited[i]){
+                return true;
+            }
+        }
+        pathVisited[node] = 0;
+        checked.push_back(node);
+        return false;
+    }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         
-        int e = graph.size();
-        vector<int> indegree(e, 0);
-        unordered_map<int, list<int>> adj;
-        for(int i=0; i<e; i++){
-            int v = i;
-            for(int u: graph[i]){
-                adj[u].push_back(v);
-                indegree[v]++;
-            }
-        }
-
-        
         unordered_map<int, bool> visited;
-        
+        unordered_map<int, bool> pathVisited;
+        vector<int> checked;
 
-        queue<int> q;
+        for(int i=0; i<graph.size(); i++){
 
-       
-        for(int i=0; i<e; i++){
-            if(indegree[i] == 0){
-                q.push(i);
-               
+            if(!visited[i]){
+                solve(i, visited, pathVisited, checked, graph);
             }
         }
 
-        vector<int> ans;
-        while(!q.empty()){
-            int top = q.front();
-            q.pop();
-            ans.push_back(top);
+        sort(checked.begin(), checked.end());
 
-            for(int i: adj[top]){
-                indegree[i]--;
-                if(indegree[i] == 0){
-                    q.push(i);
-                }
-            }
-
-        }
-
-        sort(ans.begin(), ans.end());
-
-        return ans;
+        return checked;
     }
 };
