9f33c519de596a6363cd9b884e104a209b454935
Sumit Kumar Panika <sumitkp153@gmail.com>
2026-09-03 15:11:14 +0530

    Time: 0 ms (100%), Space: 10.7 MB (23.97%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -20,7 +20,21 @@ public:
         
         int n = nums.size();
         vector<int> dp(n, -1);
+        dp[0] = nums[0];
 
-        return solve(n-1, nums, dp);
+        for(int i=1; i<n; i++){
+            int take = nums[i];
+            if(i > 1){
+                take += dp[i-2];
+            }
+
+            int notTake = 0 + dp[i-1];
+
+            dp[i] = max(take, notTake);
+        }
+
+        // return solve(n-1, nums, dp);
+
+        return dp[n-1];
     }
 };
