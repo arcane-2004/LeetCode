3fc46f4d18decbd4e6b1884bceb6fcbafdf4e781
Sumit Kumar Panika <sumitkp153@gmail.com>
2026-09-18 12:31:26 +0530

    Time: 8 ms (24.83%), Space: 23.9 MB (60.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,24 +5,20 @@ public:
 
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
-        for(int i=0; i<n; i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.empty() && end <= ans.back()[1]){
-                continue;
-            }
+        int start = intervals[0][0];
+        int end = intervals[0][1];
 
-            for(int j = i+1; j<n; j++){
-                if(intervals[j][0] <= end){
-                    end = max(end, intervals[j][1]);
-                }
-                else{
-                         
-                    break;
-                }
+        for(int i=1; i<n; i++){
+            if(end >= intervals[i][0]){
+                end = max(end, intervals[i][1]);
             }
-            ans.push_back({start, end});   
+            else{
+                ans.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
         }
+        ans.push_back({start, end});
         return ans;
     }
 };
