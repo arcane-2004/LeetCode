d5804adc79cd237c667907ca879e00a687520ac1
Sumit Kumar Panika <sumitkp153@gmail.com>
2026-09-23 12:35:28 +0530

    Time: 2 ms (88.89%), Space: 10.2 MB (25.61%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -7,19 +7,16 @@ public:
         }
 
         string say = countAndSay(n-1);
-
         string ans;
         for(int i=0; i<say.length(); i++){
             int cnt = 1;
-            char ch = say[i];
-
-            while(i < say.length() && say[i] == say[i+1]){
+            while(i+1 < say.length() && say[i] == say[i+1]){
                 cnt++;
                 i++;
             }
-
-            ans +=  to_string(cnt) + string(1, ch); 
+            ans += to_string(cnt) + say[i];
         }
+
         return ans;
     }
 };
